a86ed860272c6471e103a854d49c55440b0c173d
tptrien <tptrien95@gmail.com>
2022-06-05 16:14:07 +0700

    finished pause and play music.

--- Music-player/main.js
+++ Music-player/main.js
@@ -10,17 +10,18 @@ const player = $('.player');
 
 const app = {
     currentIndex: 0,
+    isPlaying: false,
     songs: [
         {
             name: "Click Pow Get Down",
             singer: "Raftaar x Fortnite",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
+            path: "./audio/YeuDuongKhoQuaThiChayVeKhocVoiAnh-ERIK-7128950.mp3",
             image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
         },
         {
             name: "Tu Phir Se Aana",
             singer: "Raftaar x Salim Merchant x Karma",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
+            path: "./audio/HoaTanTinhTanNewVersion-GiangJolee-7183041.mp3",
             image:
                 "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
         },
@@ -28,41 +29,41 @@ const app = {
             name: "Naachne Ka Shaunq",
             singer: "Raftaar x Brobha V",
             path:
-                "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
+                "./audio/DeVuong-DinhDungACV-7121634.mp3",
             image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
         },
         {
             name: "Mantoiyat",
             singer: "Raftaar x Nawazuddin Siddiqui",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
+            path: "./audio/ChayVeNoiPhiaAnh-KhacViet-7129688.mp3",
             image:
                 "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
         },
         {
             name: "Aage Chal",
             singer: "Raftaar",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
+            path: "./audio/ChayVeNoiPhiaAnh-KhacViet-7129688.mp3",
             image:
                 "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
         },
         {
             name: "Feeling You",
             singer: "Raftaar x Harjas",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+            path: "./audio/ChayVeNoiPhiaAnh-KhacViet-7129688.mp3",
             image:
                 "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
         },
         {
             name: "Feeling You",
             singer: "Raftaar x Harjas",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+            path: "./audio/ChayVeNoiPhiaAnh-KhacViet-7129688.mp3",
             image:
                 "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
         },
         {
             name: "Feeling You",
             singer: "Raftaar x Harjas",
-            path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+            path: "./audio/ChayVeNoiPhiaAnh-KhacViet-7129688.mp3",
             image:
                 "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
         }
@@ -90,6 +91,7 @@ const app = {
     },
 
     handleEvents: function (){
+        const _this = this;
         const cdWidth = cd.offsetWidth;
         document.onscroll = function (){
             const scrollTop = window.scrollY || document.documentElement.scrollTop
@@ -102,9 +104,16 @@ const app = {
         // Xử lý sự kiện pkay
 
         playBtn.onclick = function(){
-            // audio.play();
-            console.log(audio)
-            player.classList.add('playing');
+            if(_this.isPlaying){
+                _this.isPlaying = false;
+                audio.pause();
+                player.classList.remove('playing');
+            }else{
+                _this.isPlaying = true;
+                audio.play();
+                console.log(audio)
+                player.classList.add('playing');
+            }
         }
 
     },
@@ -112,7 +121,7 @@ const app = {
     defineProperty: function (){
         Object.defineProperty(this, 'currentSong', {
             get: function () {
-                return this.currentSong[this.currentIndex];
+                return this.songs[this.currentIndex];
             }
         })
     },
